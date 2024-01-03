30bffaa5c24c208996557798c74ef1d6f9f5c835
namgyeongmin <skarudals27@naver.com>
2024-01-04 01:04:04 +0900

    자바 입문 섹션1

---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava { // HelloJava 클래스의 범위 시작
+
+    // 자바는 실행시 main을 찾아서 실행함
+    public static void main(String[] args){ // main() 메서드의 범위 시작
+        System.out.println("hello java");
+    } // main() 메서드의 범위 끝
+} // HelloJava 클래스의 범위 끝
